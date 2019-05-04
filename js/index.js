f5679f8bb5b74e0a4598711a259c778ed9eb53f6
Teodor Grigor <contact.zeoflow@gmail.com>
2019-05-04 12:46:05 +0300

    v1.0.0

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-checkScreenSize();
-
 function onResize() {
 
 	checkScreenSize();
 
 }
+
+var userData;
+
+window.onload = function what(){
+	if (localStorage.getItem("ZeoFlowUserLogged")!==null) {
+		userData = JSON.parse(localStorage.getItem(localStorage.getItem("ZeoFlowUserLogged")));
+	}
+	checkScreenSize();
+};
 
 var action;
 var desktop;
@@ -22,6 +29,9 @@ function checkScreenSize() {
 	  		action = "../h/d/build";
 	  	} else {
 	  		if (localStorage.getItem("ZeoFlowUserLogged")!==null) {
+	  			document.getElementById('splash').hidden = true;
+	  			document.getElementById('home').hidden = false;
+				document.getElementById('txtWelcome').innerText = "Welcome, " + userData.name + "!";
 	  			action = "../h/m/home";
 			} else {
 	  			action = "../h/m/login";
